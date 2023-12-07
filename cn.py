a355bf7494088f7608a8883739515eea74d724df
yrylalala <yrylalala@163.com>
2023-12-07 13:29:12 +0800

    解决workalendar库未更新跨年数据，导致异常部署问题

--- cn.py
+++ cn.py
@@ -4,12 +4,24 @@
 from workalendar.asia import China
 from datetime import timedelta, date
 import json
+import logging
+
+from workalendar.exceptions import CalendarError
+
+# 在这里设置记录的是什么等级以上的日志
+logging.basicConfig(filename='run.log', format='%(asctime)s - %(name)s - %(levelname)s -%(module)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', level=20)
+
 if __name__ == "__main__":
     cal = China()
     cur = date.today() - timedelta(days=3)
     res = {}
     for i in range(30):
         cur += timedelta(days=1)
-        res[str(cur)] = cal.is_working_day(cur)
+        try:
+            res[str(cur)] = cal.is_working_day(cur)
+        except CalendarError:
+            logging.exception("CalendarError")
+            break
     with open('cn.json', 'w') as json_file:
         json.dump(res, json_file, indent=4, sort_keys=True)
