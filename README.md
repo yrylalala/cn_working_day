7888bf07fb6470d30e94900a24d2574e584198ba
yrylalala <yrylalala@163.com>
2026-01-05 19:15:25 +0800

    readme更新;更新日历库

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cn_working_day
-基于[Calendra](https://github.com/jaraco/calendra) 生成近14天(-2~+14)工作日信息
-（原使用[workalendar](https://github.com/workalendar/workalendar), 由于跨年之后中国假期信息更新较慢，因此更新使用Calendra）
+基于[chinese-calendar](https://github.com/LKI/chinese-calendar) 生成近14天(-2~+14)工作日信息
+（原使用[workalendar](https://github.com/workalendar/workalendar), 由于跨年之后中国假期信息更新较慢，因此更新使用chinese-calendar）
 
 使用Github action自动更新，定时任务参数参考[Github Action schedule](https://docs.github.com/zh/actions/writing-workflows/choosing-when-your-workflow-runs/events-that-trigger-workflows#schedule)
 
@@ -11,4 +11,4 @@
 fork 自仓库 [RyoLee/cn_working_day](https://github.com/RyoLee/cn_working_day)
 
 
-**注意: 由于假期每年不同，需要每年更新 [Calendra](https://github.com/jaraco/calendra) 的版本**
+**注意: 由于假期每年不同，需要每年更新 [chinese-calendar](https://github.com/LKI/chinese-calendar) 的版本**
--- cn.py
+++ cn.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from calendra.asia import China
+from chinese_calendar import is_workday
 from datetime import timedelta, date
 import json
 import logging
-
-from calendra.exceptions import CalendarError
 
 # 在这里设置记录的是什么等级以上的日志
 logging.basicConfig(filename='run.log', format='%(asctime)s - %(name)s - %(levelname)s -%(module)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', level=20)
 
 if __name__ == "__main__":
-    cal = China()
     cur = date.today() - timedelta(days=3)
     res = {}
     for i in range(16):
         cur += timedelta(days=1)
         try:
-            res[str(cur)] = cal.is_working_day(cur)
-        except CalendarError:
+            res[str(cur)] = is_workday(cur)
+        except Exception as e:
             logging.exception("CalendarError", exc_info=True)
             break
     with open('cn.json', 'w') as json_file:
